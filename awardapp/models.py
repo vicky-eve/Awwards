0e374a3b007d15b181c5be2dc77c70448f945c65
vicky <victoria.awuor@student.moringaschool.com>
2022-04-08 23:48:11 +0300

    created the profile class

--- awardapp/models.py
+++ awardapp/models.py
@@ -9,5 +9,7 @@ class Project(models.Model):
 
 class Profile(models.Model):
     username = models.TextField(max_length=50)
-    
+    bio = models.TextField(max_length=200)
+    contact_info = models.IntegerField(max_length=11)
+    profile_pic = models.ImageField(upload_to=('images/'))
     
