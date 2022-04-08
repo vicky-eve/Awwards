9ad1b2dba56c24adec8b707d0a5b675634a366bb
vicky <victoria.awuor@student.moringaschool.com>
2022-04-08 23:42:30 +0300

    created the project class

--- awardapp/models.py
+++ awardapp/models.py
@@ -1,3 +1,13 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    title = models.TextField(max_length=50)
+    landingpage_pic = models.ImageField(upload_to=('images/'), null=True)
+    description = models.TextField(max_length=200)
+
+class Profile(models.Model):
+    username = models.TextField(max_length=50)
+    
+    
