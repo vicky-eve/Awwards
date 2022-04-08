865a2d993b8b406186cd50111cdfe3f104e85d16
vicky <victoria.awuor@student.moringaschool.com>
2022-04-09 00:18:14 +0300

    created the review class

--- awardapp/models.py
+++ awardapp/models.py
@@ -1,4 +1,5 @@
 from distutils.command.upload import upload
+from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -8,6 +9,8 @@ class Project(models.Model):
     landingpage_pic = models.ImageField(upload_to=('images/'), null=True)
     description = models.TextField(max_length=200)
     date_posted = models.DateTimeField(auto_now_add=True,blank=True)
+    link = models.URLField(max_length=300)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='project')
 
 class Profile(models.Model):
     username = models.TextField(max_length=50, blank=True)
@@ -15,3 +18,10 @@ class Profile(models.Model):
     contact_info = models.IntegerField(max_length=11)
     profile_pic = models.ImageField(upload_to=('images/'))
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+
+class Review(models.Model):
+    usability = models.IntegerField()
+    design = models.IntegerField()
+    creativity = models.IntegerField()
+    content = models.IntegerField
+    developer = models.IntegerField()
