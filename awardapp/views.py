16ddeee7bd041a8351599689829904546a5cba9d
vicky <victoria.awuor@student.moringaschool.com>
2022-04-09 00:35:07 +0300

    index view for displaying the projects on the index page

--- awardapp/views.py
+++ awardapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from awardapp.models import Project, Profile
+
 # Create your views here.
+def index(request):
+    """
+    display all projects and profiles
+    """
+    projects = Project.objects.all
+    profiles = Profile.objects.all
+
+    return render(request, 'index.html', {'projects':projects, 'profiles':profiles})
+
