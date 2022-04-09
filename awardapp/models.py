7d89d3177255fc52bb62675646fc74cca26ba4b5
vicky <victoria.awuor@student.moringaschool.com>
2022-04-09 14:19:06 +0300

    updated the reviw class to have defaults

--- awardapp/models.py
+++ awardapp/models.py
@@ -1,5 +1,5 @@
 from distutils.command.upload import upload
-from tkinter import CASCADE
+from phone_field import PhoneField
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -19,7 +19,7 @@ class Project(models.Model):
 class Profile(models.Model):
     username = models.TextField(max_length=50, blank=True)
     bio = models.TextField(max_length=200, blank=True)
-    contact_info = models.IntegerField(max_length=11)
+    contact_info = PhoneField(max_length=11, blank=True)
     profile_pic = models.ImageField(upload_to=('images/'))
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
 
@@ -28,8 +28,10 @@ class Profile(models.Model):
         return cls.objects.filter(user__username__icontains=user).all()
 
 class Review(models.Model):
-    usability = models.IntegerField()
-    design = models.IntegerField()
-    creativity = models.IntegerField()
-    content = models.IntegerField
-    developer = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='review')
+    usability = models.IntegerField(default=0)
+    design = models.IntegerField(default=0)
+    creativity = models.IntegerField(default=0)
+    content = models.IntegerField(default=0)
+    developer = models.IntegerField(default=0)
+   
