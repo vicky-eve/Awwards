57bc6d5de4529849426e6e9a18de47c83ad8ebb9
vicky <victoria.awuor@student.moringaschool.com>
2022-04-09 23:21:02 +0300

    api url for deleting the model classes

--- awardapp/urls.py
+++ awardapp/urls.py
@@ -7,7 +7,7 @@ urlpatterns= [
     path('api/project/', views.ProjectList.as_view()),
     path('api/profile/', views.ProfileList.as_view()),
     path('api/review/', views.ReviewList.as_view()),
-    path('api/merch/merch-id/(?P<pk>[0-9]+)/',views.ProjectDescription.as_view()),
-    path('api/merch/merch-id/(?P<pk>[0-9]+)/',views.ProfileDescription.as_view()),
-    path('api/merch/merch-id/(?P<pk>[0-9]+)/',views.ReviewDescription.as_view()),
+    path('api/merch/project-id/(?P<pk>[0-9]+)/',views.ProjectDescription.as_view()),
+    path('api/merch/profile-id/(?P<pk>[0-9]+)/',views.ProfileDescription.as_view()),
+    path('api/merch/review-id/(?P<pk>[0-9]+)/',views.ReviewDescription.as_view()),
 ]
