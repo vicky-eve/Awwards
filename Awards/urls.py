0f121550ff42959e746e42264f840b3a0fbd72eb
vicky <victoria.awuor@student.moringaschool.com>
2022-04-09 15:42:49 +0300

    added authtokens to project urls

--- Awards/urls.py
+++ Awards/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +24,5 @@ urlpatterns = [
     path('accounts/',include('django.contrib.auth.urls')),
     path('registration/', include('registration.backends.simple.urls')),
     path('logout/', views.logout_then_login), 
+    path('api-token-auth/', obtain_auth_token)
 ]
