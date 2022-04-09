87f037edd7d2ab1c67e329749f96f3e07e2eeb2f
vicky <victoria.awuor@student.moringaschool.com>
2022-04-09 14:20:50 +0300

    added urls for the api serializers

--- awardapp/urls.py
+++ awardapp/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns= [
     path('', views.index, name='index'),
     path('index/', views.index, name='index'),
+    path('^api/project/$', views.ProjectList.as_view()),
+    path('^api/profile/$', views.ProfileList.as_view()),
+    path('^api/review/$', views.ReviewList.as_view()),
 ]
