d66b8d7fc0fe157fb8504b47811082333657dd8f
vicky <victoria.awuor@student.moringaschool.com>
2022-04-09 15:40:21 +0300

    added the  id field

--- awardapp/serializer.py
+++ awardapp/serializer.py
@@ -4,14 +4,14 @@ from .models import Profile, Project, Review
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
-        fields = ('title', 'landingpage_pic', 'description', 'link')
+        fields = ('id', 'title', 'landingpage_pic', 'description', 'link')
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ('username', 'bio', 'contact_info', 'profile_pic')
+        fields = ('id', 'username', 'bio', 'contact_info', 'profile_pic')
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
-        fields = ('usability', 'design', 'creativity', 'content', 'developer')
+        fields = ('id', 'usability', 'design', 'creativity', 'content', 'developer')
