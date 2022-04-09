36aa2499fd7e325d2b03bd468b8b1b5787417c6d
vicky <victoria.awuor@student.moringaschool.com>
2022-04-09 15:41:48 +0300

    added post, update and delete methods

--- awardapp/views.py
+++ awardapp/views.py
@@ -4,6 +4,9 @@ from awardapp.models import Project, Profile, Review
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializer import ProfileSerializer, ProjectSerializer, ReviewSerializer
+from rest_framework import status
+from .permissions import IsAdminOrReadOnly
+from django.http import Http404
 
 #api views
 class ProjectList(APIView):
@@ -11,6 +14,43 @@ class ProjectList(APIView):
         all_projects = Project.objects.all()
         serializers = ProjectSerializer(all_projects, many=True)
         return Response(serializers.data)
+        
+
+    def post(self, request, format=None):
+        serializers = ProjectSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    permission_classes = (IsAdminOrReadOnly,)
+
+class ProjectDescription(APIView):
+    permission_classes = (IsAdminOrReadOnly,)
+    def get_project(self, pk):
+        try:
+            return Project.objects.get(pk=pk)
+        except Project.DoesNotExist:
+            return Http404
+
+    def get(self, request, pk, format=None):
+        project = self.get_project(pk)
+        serializers = ProjectSerializer(project)
+        return Response(serializers.data)
+
+    def put(self, request, pk, format=None):
+        project = self.get_project(pk)
+        serializers = ProjectSerializer(project, request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        else:
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        project = self.get_project(pk)
+        project.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class ProfileList(APIView):
     def get(self, request, format=None):
@@ -18,11 +58,83 @@ class ProfileList(APIView):
         serializers = ProfileSerializer(all_profiles, many=True)
         return Response(serializers.data)
 
+    def post(self, request, format=None):
+        serializers = ProfileSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    permission_classes = (IsAdminOrReadOnly,)
+
+class ProfileDescription(APIView):
+    permission_classes = (IsAdminOrReadOnly,)
+    def get_profile(self, pk):
+        try:
+            return Profile.objects.get(pk=pk)
+        except Profile.DoesNotExist:
+            return Http404
+
+    def get(self, request, pk, format=None):
+        profile = self.get_profile(pk)
+        serializers = ProfileSerializer(profile)
+        return Response(serializers.data)
+
+    def put(self, request, pk, format=None):
+        profile = self.get_profile(pk)
+        serializers = ProfileSerializer(profile, request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        else:
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        profile = self.get_profile(pk)
+        profile.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 class ReviewList(APIView):
     def get(self, request, format=None):
         all_reviews = Review.objects.all()
         serializers = ReviewSerializer(all_reviews, many=True)
         return Response(serializers.data)
+
+    def post(self, request, format=None):
+        serializers = ReviewSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    permission_classes = (IsAdminOrReadOnly,)
+
+    
+
+class ReviewDescription(APIView):
+    permission_classes = (IsAdminOrReadOnly,)
+    def get_review(self, pk):
+        try:
+            return Review.objects.get(pk=pk)
+        except Review.DoesNotExist:
+            return Http404
+
+    def get(self, request, pk, format=None):
+        review = self.get_review(pk)
+        serializers = ReviewSerializer(review)
+        return Response(serializers.data)
+
+    def put(self, request, pk, format=None):
+        review = self.get_review(pk)
+        serializers = ReviewSerializer(review, request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        else:
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        review = self.get_review(pk)
+        review.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 # Create your views here.
 @login_required(login_url='/registration/login/')
