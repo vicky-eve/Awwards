7c08738ad44e42f92529f6a1cf299b0d6ba4851d
vicky <victoria.awuor@student.moringaschool.com>
2022-04-09 15:41:02 +0300

    added url paths  for getting specific classes

--- awardapp/urls.py
+++ awardapp/urls.py
@@ -4,7 +4,10 @@ from . import views
 urlpatterns= [
     path('', views.index, name='index'),
     path('index/', views.index, name='index'),
-    path('^api/project/$', views.ProjectList.as_view()),
-    path('^api/profile/$', views.ProfileList.as_view()),
-    path('^api/review/$', views.ReviewList.as_view()),
+    path('api/project/', views.ProjectList.as_view()),
+    path('api/profile/', views.ProfileList.as_view()),
+    path('api/review/', views.ReviewList.as_view()),
+    path('api/merch/merch-id/(?P<pk>[0-9]+)/',views.ProjectDescription.as_view()),
+    path('api/merch/merch-id/(?P<pk>[0-9]+)/',views.ProfileDescription.as_view()),
+    path('api/merch/merch-id/(?P<pk>[0-9]+)/',views.ReviewDescription.as_view()),
 ]
