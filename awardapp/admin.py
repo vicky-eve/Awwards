6de6e86ff392a50d78e4f60a8c890790e4a7ab7c
vicky <victoria.awuor@student.moringaschool.com>
2022-04-09 14:18:19 +0300

    registering the model classes

--- awardapp/admin.py
+++ awardapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from awardapp.models import Profile, Project, Review
+
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(Profile)
+admin.site.register(Review)
